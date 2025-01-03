e83bc606078fdade48f51fbb37dd005f16940a35
Josen Jose <josen.jose@soti.net>
2025-01-03 22:12:51 +0530

    cleanup changes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/shared/Header"
+
+export default function InventoryPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <dashboard /> */}
+      </main>
+    </>
+  );
+}
